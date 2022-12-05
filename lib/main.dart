78c8f98f3f9c99794f8f7c1c5869c952d10b7305
Natasya Nadhira <natasyanadhira51@gmail.com>
2022-12-05 17:13:23 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:utmexpress/pages/splash.dart';
 import 'package:utmexpress/pages/loading.dart';
 import 'package:utmexpress/pages/login.dart';
 import 'package:utmexpress/pages/home.dart';
@@ -7,7 +8,8 @@ import 'package:utmexpress/pages/home.dart';
 void main() => runApp(MaterialApp(
   initialRoute: '/login',
   routes: {
-    '/': (context) => Loading(),
+    '/': (context) => Splash(),
+    '/loading': (context) => Loading(),
     '/login': (context) => Login(),
     '/home': (context) => Home(),
   },
