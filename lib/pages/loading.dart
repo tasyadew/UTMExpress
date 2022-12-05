5dbf6018c0f54db730b0c3e4b3363c566a9f9b71
Natasya Nadhira <natasyanadhira51@gmail.com>
2022-12-05 17:13:05 +0800

    Update loading.dart

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:utmexpress/shared/color.dart';
 
-class Loading extends StatelessWidget {
+class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
 
   @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: MyColor.maroon,
+    return Scaffold(
+      backgroundColor: MyColor.maroon[400],
       body: Center(
-          child: Text(
-            'UTMExpress',
-            style: TextStyle(
-              fontSize: 48,
-              fontFamily: 'Arial',
-              color: MyColor.amber,
-            ),
-          ),
+        child: SpinKitFadingCube(
+          color: MyColor.amber[100],
+          size:50.0,
+        ),
       ),
     );
   }
